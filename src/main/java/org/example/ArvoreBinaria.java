4eeb1601c1b78b5ba3a83001ca01d7961f1405f5
Gabriel Koga <gabrielkoga1208@gmail.com>
2026-04-29 20:19:17 -0300

    atualização do finalzin da aula colocando um construtorar da arvor binari

--- src/main/java/org/example/ArvoreBinaria.java
+++ src/main/java/org/example/ArvoreBinaria.java
@@ -4,4 +4,13 @@ public class ArvoreBinaria {
 
     No raiz;
 
+    public ArvoreBinaria(){
+        this.raiz = new No(null);
+        System.out.println("Arvore plantada");
+    }
+
+    public void inserir(Integer conteudo){
+
+    }
+
 }
